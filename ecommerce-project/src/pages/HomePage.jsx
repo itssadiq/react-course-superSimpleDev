ecf464c186c7e7440d73b09b239e421eb48d0a7d
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-14 11:17:03 +0500

    used data from the backend to generate products on  homepage

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { Header } from "../components/Header";
-import { Helmet } from "react-helmet";
-import { products } from "../../starting code/data/products";
 import "./HomePage.css";
+import { useEffect, useState } from "react";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
