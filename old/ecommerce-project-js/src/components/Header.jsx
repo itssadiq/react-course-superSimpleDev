b668f4bbf206f8bdb7e0f723f8d783c02998e7cc
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-22 15:29:19 +0500

    Converted E-commerce project into typescript

---
+++ old/ecommerce-project-js/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import { Link, NavLink, useNavigate, useSearchParams } from "react-router";
+import LogoWhite from "../assets/images/logo-white.png";
+import MobileLogoWhite from "../assets/images/mobile-logo-white.png";
+import SearchIcon from "../assets/images/icons/search-icon.png";
+import CartIcon from "../assets/images/icons/cart-icon.png";
+import "./Header.css";
+import { useState } from "react";
+
+export function Header({ cart }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
+  const [searchText, setSearchText] = useState(search || "");
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
+
+    navigate(`/?search=${searchText}`);
+  };
+
+  let totalQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
+  return (
+    <>
+      <div className="header">
+        <div className="left-section">
+          <NavLink to="/" className="header-link">
+            <img className="logo" src={LogoWhite} />
+            <img className="mobile-logo" src={MobileLogoWhite} />
+          </NavLink>
+        </div>
+
+        <div className="middle-section">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search"
+            onChange={handleSearch}
+          />
+
+          <button className="search-button">
+            <img className="search-icon" src={SearchIcon} />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <NavLink className="orders-link header-link" to="/orders">
+            <span className="orders-text">Orders</span>
+          </NavLink>
+
+          <NavLink className="cart-link header-link" to="/checkout">
+            <img className="cart-icon" src={CartIcon} />
+            <div className="cart-quantity">{totalQuantity}</div>
+            <div className="cart-text">Cart</div>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ old/ecommerce-project-js/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router";
+import axios from "axios";
+import { Header } from "../../components/Header";
+import "./OrdersPage.css";
+import { Fragment, useEffect, useState } from "react";
+import OrdersGrid from "./OrdersGrid";
+
+export function OrdersPage({ cart, loadCart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrdersData = async () => {
+      const response = await axios.get("/api/orders?expand=products");
+
+      setOrders(response.data);
+    };
+
+    fetchOrdersData();
+  }, []);
+  return (
+    <>
+      <title>Orders</title>
+      <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
+      <Header cart={cart} />
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <OrdersGrid orders={orders} loadCart={loadCart} />
+      </div>
+    </>
+  );
+}
